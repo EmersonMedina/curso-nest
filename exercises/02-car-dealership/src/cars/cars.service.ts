4a03cfc975136007ef1f244485fd1a00d8e3e83f
EmersonMedina <emermedina877@gmail.com>
2023-10-08 21:25:50 -0600

    Sección 5

--- exercises/02-car-dealership/src/cars/cars.service.ts
+++ exercises/02-car-dealership/src/cars/cars.service.ts
@@ -7,21 +7,11 @@ import { CreateCarDto, UpdateCarDto } from './dtos';
 export class CarsService {
 
   private cars: Car[] = [
-    {
-        id: uuid(),
-        brand: 'Toyota',
-        model: 'Corolla'
-    }, 
-    {
-        id: uuid(),
-        brand: 'Honda',
-        model: 'Civic'
-    }, 
-    {
-        id:uuid(), 
-        brand: 'Jeep',
-        model: 'Cherokee'
-    }
+    // {
+    //     id: uuid(),
+    //     brand: 'Toyota',
+    //     model: 'Corolla'
+    // }, 
   ];
 
   findAll() {
@@ -63,4 +53,7 @@ export class CarsService {
     this.cars = this.cars.filter(car => car.id !== id); 
   }
 
+  fillCarsWithSeed(cars: Car[]) {
+    this.cars = cars;
+  }
 }
